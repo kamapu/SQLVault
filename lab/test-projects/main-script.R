71fa4f25fa69cb0f0102d99b330e0ea48a431f8e
Miguel Alvarez <kamapu@posteo.de>
2026-01-03 14:06:43 +0100

    Data added to iris test db.

--- lab/test-projects/main-script.R
+++ lab/test-projects/main-script.R
@@ -2,13 +2,16 @@ library(SQLVault)
 
 # Initialize Project
 init_project(dbname = "iris-db", user = "miguel",
-    path = "lab/test-projects/new-project",
+    path = "lab/test-projects/new-project-2",
     restore_from = "lab/test-projects/iris-backups")
 
 # Release Project
 release_project(
-    project_path = "lab/test-projects/new-project",
+    project_path = "lab/test-projects/new-project-2",
     backup_path = "lab/test-projects/iris-backups",
     vault = "lab/test-projects/iris-backups/vault",
-    release_sql = "create-iris-db.sql"
+    release_sql = "update_query.sql"
 )
+
+# Update Project
+do_restore()
